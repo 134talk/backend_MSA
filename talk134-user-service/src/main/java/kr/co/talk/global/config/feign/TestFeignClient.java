4c8bf0abdf7bc218026d792b37c6ef78efd9bab1
jhkim593 <fpdlwjzlr@naver.com>
2023-05-11 02:56:29 +0900

    openfeignclient 설정

--- talk134-user-service/src/main/java/kr/co/talk/global/config/feign/TestFeignClient.java
+++ talk134-user-service/src/main/java/kr/co/talk/global/config/feign/TestFeignClient.java
@@ -1,7 +1,11 @@
 package kr.co.talk.global.config.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient
-public class TestFeignClient {
+@FeignClient(value = "test", url = "testUrl")
+public interface TestFeignClient {
+    @GetMapping("/test")
+    ResponseEntity getTest();
 }
